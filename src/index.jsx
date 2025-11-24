0eb69978355169ffd447c2428c6e9febabfbeea8
helga-umrikh <zetaumr@gmail.com>
2025-11-24 08:14:44 +0300

    fix: path to build

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
     <React.StrictMode>
         <Provider store={store}>
-            <Router>
+            <Router basename='/Free-To-Play-Games'>
                 <App />
             </Router>
         </Provider>
